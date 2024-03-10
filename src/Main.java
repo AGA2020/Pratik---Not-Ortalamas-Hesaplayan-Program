25e125f65c7812f929d4d3929e66f3cb8696f579
AGA2020 <hasan9u@gmail.com>
2024-03-10 17:17:33 +0300

    Proje bitti

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,30 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //Değişkenleri oluştur
+        int mat, fizik, kimya, turkce, tarih, muzik;
+        // Scanner sınıfımızı tanımladık
+        Scanner inp = new Scanner(System.in);
+        // Kullanıcıdan değerleri al
+        System.out.print("Matematik Notunuz : ");
+        mat = inp.nextInt();
+        System.out.print("Fizik Notunuz : ");
+        fizik = inp.nextInt();
+        System.out.print("Kimya Notunuz : ");
+        kimya = inp.nextInt();
+        System.out.print("Türkçe Notunuz : ");
+        turkce = inp.nextInt();
+        System.out.print("Tarih Notunuz : ");
+        tarih = inp.nextInt();
+        System.out.print("Müzik Notunuz : ");
+        muzik = inp.nextInt();
 
+        int toplam = (mat + fizik + kimya + turkce + tarih + muzik);
+        double sonuc = toplam / 6.0;
+        System.out.println("Ortalamanız : " + sonuc);
+        String str = (sonuc > 60) ? "Sınıfı Geçti" : "Sınıfta Kaldı";
+        System.out.print(str);
     }
 }
